be89397c4320c3186c3f30375e961b429ef5d524
Pradyumna Kalaskar <pradyumnakalaskar43@gmail.com>
2024-09-07 11:14:34 +0530

    updated project

--- frontend/src/components/layout/Offer.jsx
+++ frontend/src/components/layout/Offer.jsx
@@ -4,6 +4,12 @@ import { motion } from "framer-motion";
 function Offer() {
   const [width, setWidth] = useState(0);
   const carousel = useRef();
+
+  const gradientStyle = {
+    background: "linear-gradient(to bottom, #FF8C00, #FFA500)",
+    minWidth: "18.5rem",
+  };
+
   return (
     <>
       <div className="mt-0 pt-0">
@@ -14,21 +20,26 @@ function Offer() {
         >
           <motion.div
             drag="x"
-            dragConstraints={{ right: 0, left: -320 }}
+            dragConstraints={{ right: 0, left: -300 }}
             className="inner-carousel"
           >
             {" "}
-            <div className="Offer m-2 rounded-md text-white mt-2 min-w-84 p-3 h-28">
+            <div
+              style={gradientStyle}
+              className="Offer m-2 rounded-md text-white h-28"
+            >
               <p className="font-bold text-lg mb-0">Happy Ganesh Chaturthi</p>
               <p className="text-base mb-0 font-medium">
-                Use code GANESHA on Orders above &#8377;350 and get &#8377;30
-                Off
+                Use code GANESHA on Order above &#8377;350 and get &#8377;30 Off
               </p>
             </div>
-            <div className="Offer m-2 rounded-md text-white mt-2 min-w-84 h-28">
+            <div
+              style={{ minWidth: "20rem" }}
+              className="Offer m-2 rounded-md text-white h-28"
+            >
               <p className="font-bold text-lg mb-0">Get &#8377;30 Off</p>
               <p className="text-base mb-0 font-medium">
-                Use code SAVE30 on Orders above &#8377;350 <br />
+                Use code SAVE30 on Order above &#8377;350 <br />
                 Today
               </p>
             </div>
